f2cd8a934d5685f813862859ef9e8e19fbeb9592
Nilesh <nilesh@londhe.com>
2013-04-27 23:16:19 -0700

    reorg

--- client/client.js
+++ client/client.js
@@ -1,12 +1,7 @@
-if (Meteor.isClient) {
+draw = function() {
   Template.svg.rendered = function () {
     d3.select("body")
       .append("p")
       .text("Hello d3");
   };
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-  });
-}
+};
